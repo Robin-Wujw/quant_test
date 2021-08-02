1afa15f1c2c0bc2f5c0504764893bc2b7c4b7f44
Robinwu <robin.wujw@gmail.com>
2021-08-03 00:18:54 +0800

    fix

--- 2.py
+++ 2.py
@@ -28,7 +28,25 @@ df = df.dropna()
 # print(death_cross) 
 sr1 = df['ma5'] < df['ma30']
 sr2 = df['ma5'] > df['ma30']
-
 death_cross = df[sr1 & sr2.shift(1)].index
 golden_cross = df[~(sr1 | sr2.shift(1))].index
-print(golden_cross)
+# print(golden_cross)
+first_money = 100000
+money = first_money
+sr1 = pd.Series(1,index=golden_cross)
+sr2 = pd.Series(0,index=death_cross)
+sr = sr1.append(sr2).sort_index()
+hold = 0 #持有多少股
+for i in range(0,len(sr)):
+    p = df['开盘'][sr.index[i]]
+    if sr.iloc[i] == 1:
+        #金叉，买入
+        buy = money // (100*p)
+        hold += buy*100
+        money -= buy*100*p
+    else:
+        money += hold * p
+        hold = 0
+p = df['开盘'][-1]
+now_money = hold*p + money
+print(now_money - first_money)
